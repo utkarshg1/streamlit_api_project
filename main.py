8efe77c7318b45006f004d70e7f627376b8a6168
utkarshg1 <gaikwadujg@gmail.com>
2025-05-10 17:53:49 +0530

    streamlit app done

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from api-project!")
+# Streamlit app
+import streamlit as st
+from utils import StockAPI
 
+# Set page config
+st.set_page_config(page_title="Stock App", layout="wide")
 
-if __name__ == "__main__":
-    main()
+# Get the stockapi
+client = StockAPI()
+
+# Add a title to web app
+st.title("Stock Market App")
+
+# Add a subheading
+st.subheader("By Utkarsh Gaikwad")
+
+# Take text input from user
+company = st.text_input("Company Name :")
+
+# If company name is entered show some stock symbols and information
+if company:
+    r1 = client.symbol_search(company)
+    dropdown = st.selectbox("Symbol", options=r1["1. symbol"].tolist())
+    company_details = r1[r1["1. symbol"] == dropdown]
+    st.dataframe(company_details)
+
+    # Create a submit button
+    submit = st.button("Submit", type="primary")
+
+    # If submit button clicked plot the chart
+    if submit:
+        df = client.stock_data(dropdown)
+        fig = client.plot_chart(df)
+        st.plotly_chart(fig)
